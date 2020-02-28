874709822657097666c6608f503a2d8fcfc883bd
pydemia <pydemia@gmail.com>
2020-02-29 00:20:59 +0900

    write a tfrecord converter

---
+++ scripts/python/mscoco-jpg-to-tfrecord.py
@@ -0,0 +1,97 @@
+# %% Setup
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+try:
+  # %tensorflow_version only exists in Colab.
+  !pip install -q tf-nightly
+except Exception:
+  pass
+import tensorflow as tf
+
+import numpy as np
+import IPython.display as display
+
+
+# %% `tf.Example`
+
+# The following functions can be used to convert a value to a type compatible
+# with tf.Example.
+
+def _bytes_feature(value):
+  """Returns a bytes_list from a string / byte."""
+  if isinstance(value, type(tf.constant(0))):
+    value = value.numpy() # BytesList won't unpack a string from an EagerTensor.
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+def _float_feature(value):
+  """Returns a float_list from a float / double."""
+  return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
+def _int64_feature(value):
+  """Returns an int64_list from a bool / enum / int / uint."""
+  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+print(_bytes_feature(b'test_string'))
+print(_bytes_feature(u'test_bytes'.encode('utf-8')))
+
+print(_float_feature(np.exp(1)))
+
+print(_int64_feature(True))
+print(_int64_feature(1))
+
+
+feature = _float_feature(np.exp(1))
+
+feature.SerializeToString()
+
+# %% Creating a `tf.Example` Message
+
+# The number of observations in the dataset.
+n_observations = int(1e4)
+
+# Boolean feature, encoded as False or True.
+feature0 = np.random.choice([False, True], n_observations)
+
+# Integer feature, random from 0 to 4.
+feature1 = np.random.randint(0, 5, n_observations)
+
+# String feature
+strings = np.array([b'cat', b'dog', b'chicken', b'horse', b'goat'])
+feature2 = strings[feature1]
+
+# Float feature, from a standard normal distribution
+feature3 = np.random.randn(n_observations)
+
+
+def serialize_example(feature0, feature1, feature2, feature3):
+  """
+  Creates a tf.Example message ready to be written to a file.
+  """
+  # Create a dictionary mapping the feature name to the tf.Example-compatible
+  # data type.
+  feature = {
+      'feature0': _int64_feature(feature0),
+      'feature1': _int64_feature(feature1),
+      'feature2': _bytes_feature(feature2),
+      'feature3': _float_feature(feature3),
+  }
+
+  # Create a Features message using tf.train.Example.
+
+  example_proto = tf.train.Example(features=tf.train.Features(feature=feature))
+  return example_proto.SerializeToString() 
+
+
+# This is an example observation from the dataset.
+
+example_observation = []
+
+serialized_example = serialize_example(False, 4, b'goat', 0.9876)
+serialized_example
+
+
+example_proto = tf.train.Example.FromString(serialized_example)
+example_proto
+
